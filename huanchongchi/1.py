5734a30b244d0e2c00f2bd5b87686ddae2611f8d
hadesgo <871886736@qq.com>
2020-11-14 19:56:26 +0800

    git push -u origin main

---
+++ huanchongchi/1.py
@@ -0,0 +1,10 @@
+''' 
+* @Author: hades-li  
+* @Date: 2020-11-14 17:11:09  
+* @Last Modified by:   hades-li  
+* @Last Modified time: 2020-11-14 17:11:09
+'''
+#coding: UTF-8
+
+print("我是你的爸爸啊")
+print("hello world")
